569e083bdead7ce3a4373376a49b762d1315d3b5
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-07-25 16:47:12 +0200

    Finished Most Activites

--- app/src/main/java/com/example/scientia/HomeActivity.kt
+++ app/src/main/java/com/example/scientia/HomeActivity.kt
@@ -2,10 +2,16 @@ package com.example.scientia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputLayout
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val til : TextInputLayout = findViewById(R.id.layout);
+        val input : EditText = findViewById(R.id.name_input);
+        val text = input.getText().length;
     }
 }
