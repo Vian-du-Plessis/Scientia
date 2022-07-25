e3104541d5eb4c0664046632744701e6868bb321
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-07-25 11:34:35 +0200

    HomeActivity Done

---
+++ app/src/main/java/com/example/scientia/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.scientia
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val backgroundImage : ImageView = findViewById(R.id.loader);
+        val animation = AnimationUtils.loadAnimation( this,R.anim.loader );
+        backgroundImage.startAnimation(animation);
+
+        Handler().postDelayed({
+            startActivity(Intent(this,HomeActivity::class.java));
+            finish();
+        }, 5000);
+    }
+}
