3083f6093179f4de50501e65b1773c3477d975e8
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-08-04 10:00:02 +0200

    Added Navigation from Home to Category

--- app/src/main/java/com/example/scientia/CategoryActivity.kt
+++ app/src/main/java/com/example/scientia/CategoryActivity.kt
@@ -6,11 +6,11 @@ import android.view.View
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.MobileAds
-import kotlinx.android.synthetic.main.activity_category.*
 
 class CategoryActivity : AppCompatActivity() {
 
-    lateinit var mAdView : AdView
+    private lateinit var mAdView : AdView;
+    private lateinit var binding: CategoryActivity;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/scientia/HomeActivity.kt
+++ app/src/main/java/com/example/scientia/HomeActivity.kt
@@ -1,17 +1,37 @@
 package com.example.scientia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.Toast
+import com.example.scientia.databinding.ActivityHomeBinding
 import com.google.android.material.textfield.TextInputLayout
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val til : TextInputLayout = findViewById(R.id.layout);
-        val input : EditText = findViewById(R.id.name_input);
-        val text = input.getText().length;
+        binding = ActivityHomeBinding.inflate(layoutInflater);
+
+        setContentView(binding.root);
+
+        binding.acbBtnStart.setOnClickListener {
+            val username = binding.etNameInput.text;
+            val inputLayout = binding.tfTextInputLayout;
+            if ( username.length > 8 ) {
+                inputLayout.setError("This name is too long.");
+            } else if ( username.length < 1 ) {
+                inputLayout.setError("Please enter your name.")
+            } else {
+                val intent = Intent(this, CategoryActivity::class.java);
+                startActivity(intent);
+            }
+        }
+
     }
 }
