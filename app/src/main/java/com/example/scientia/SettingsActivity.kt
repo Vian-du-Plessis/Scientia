d4d9135047b9ad61c9e63c7b9044ac04e89481f9
Vian-du-Plessis <21100483@virtualwindow.co.za>
2022-08-10 22:25:25 +0200

    Initialised Ads on SettingsActivity

--- app/src/main/java/com/example/scientia/SettingsActivity.kt
+++ app/src/main/java/com/example/scientia/SettingsActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.scientia.databinding.ActivitySettingsBinding
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class SettingsActivity : AppCompatActivity() {
 
+    private lateinit var mAdView: AdView;
     private lateinit var binding: ActivitySettingsBinding;
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +21,13 @@ class SettingsActivity : AppCompatActivity() {
 
         setContentView(binding.root);
 
+        // Initialise Ads
+        MobileAds.initialize(this){};
+        mAdView = findViewById(R.id.adView);
+        val adRequest = AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // Navigates Back to Home Page
         binding.ivBackbtn.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java);
             startActivity(intent);
